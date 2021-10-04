1ab7a0155696b4ea625f6b2c8e7d4ff8df5e815c
alecF2 <alec@null.net>
2021-10-04 12:35:53 -0700

    allow backend to take in files for image/sound upload

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,25 @@
 import express from 'express'
 import * as r from 'rethinkdb'
+import multer from 'multer'
 
 const port = 4000
 const app = express()
+const upload = multer()
+
 let connection: r.Connection
 
-app.post("/add_item", (req, res) => {
+app.use(express.json())
+
+app.post("/add_item", upload.single('image'), (req, res) => {
+  // console.log(req.body)
+  // console.log(req.file)
   r.db('a1').table('vocab_items').insert({
-    language: "Spanish",
-    translation: "book"
+    word: req.body.word,
+    language: req.body.language,
+    translation: req.body.translation,
+    example: req.body.example,
+    image: req.file!.buffer,
+    image_desc: req.body.image_desc
   }).run(connection, (err, result) => {
     if (err) throw err
     console.log(JSON.stringify(result, null, 2))
@@ -24,6 +35,7 @@ app.listen(port, () => {
   }, function(err, conn) {
     if (err) throw err
     connection = conn
+    console.log("connected to rethinkDB!")
   })
 })
 
