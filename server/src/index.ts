356f2dae68fcfc60113569d5730b8b297750e9df
alecF2 <alec@null.net>
2021-10-09 00:19:34 -0700

    refactored server to await DB connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,8 +7,6 @@ const port = 4000
 const app = express()
 const upload = multer()
 
-let connection: r.Connection
-
 app.use(express.static(join(__dirname, "..", "..", "build")))
 app.use(express.json())
 
@@ -23,48 +21,60 @@ const fields = [
   }
 ]
 
-app.post("/add_item", upload.fields(fields), (req, res) => {
-  // console.log(req.body)
-  console.log(req.files)
-  const files = req.files as Record<string, Express.Multer.File[]> | undefined
-  // console.log(files.image[0])
-  // console.log(files.pronunciation[0])
-  r.db('a1').table('vocab_items').insert({
-    word: req.body.word,
-    language: req.body.language,
-    translation: req.body.translation,
-    example: req.body.example,
-    image: {
-      mimetype: files?.image[0].mimetype,
-      buffer: files?.image[0].buffer,
-      originalname: files?.image[0].originalname
-    },
-    image_desc: req.body.imageDesc,
-    pronunciation: {
-      mimetype: files?.pronunciation[0].mimetype,
-      buffer: files?.pronunciation[0].buffer,
-      originalname: files?.pronunciation[0].originalname
+app.on("ready", () => {
+  app.post("/add_item", upload.fields(fields), async (req, res) => {
+    console.log(req.files)
+    const files = req.files as Record<string, Express.Multer.File[]> | undefined
+
+    const data = {
+      word: req.body.word,
+      language: req.body.language,
+      translation: req.body.translation,
+      example: req.body.example,
+      image: {
+        mimetype: files?.image[0].mimetype,
+        buffer: files?.image[0].buffer,
+        originalname: files?.image[0].originalname
+      },
+      image_desc: req.body.imageDesc,
+      pronunciation: {
+        mimetype: files?.pronunciation[0].mimetype,
+        buffer: files?.pronunciation[0].buffer,
+        originalname: files?.pronunciation[0].originalname
+      }
     }
-  }).run(connection, (err, result) => {
-    if (err) throw err
-    console.log(JSON.stringify(result, null, 2))
-    res.send("success!")
+
+    let result: r.WriteResult
+    try {
+      result = await r.db('a1').table('vocab_items').insert(data).run(connection)
+      console.log(result)
+      res.send("successfully added vocabulary item!")
+    } catch (err) {
+      console.log(err)
+      res.send("could not add vocabulary item")
+    }
+  })
+
+  app.use((req, res) => {
+    res.sendFile(join(__dirname, "..", "..", "build", "index.html"))
+  })
+
+  app.listen(port, async () => {
+    console.log("listening on port " + port)
   })
 })
 
-app.use((req, res) => {
-  res.sendFile(join(__dirname, "..", "..", "build", "index.html"))
-})
 
-app.listen(port, () => {
-  console.log("listening on port " + port)
-  r.connect({
-    host: 'localhost',
-    port: 28015
-  }, function (err, conn) {
-    if (err) throw err
-    connection = conn
-    console.log("connected to rethinkDB!")
-  })
+let connection: r.Connection
+r.connect({
+  host: "localhost",
+  port: 28015
+}, (err, conn) => {
+  if (err) {
+    console.log(err)
+    throw err
+  }
+  connection = conn
+  console.log("connected to rethinkDB!")
+  app.emit("ready")
 })
-
